e331daecb7d3080a210b595054c56054b3110eee
skansher <117953654+skansher@users.noreply.github.com>
2025-02-21 17:12:13 -0500

    resume content part 1

--- my-app/src/Components/Resume.jsx
+++ my-app/src/Components/Resume.jsx
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./Header";
 import '../CSS/website.css';
 import '../CSS/resume.css';
+import resumeData from '../Files/resume.json';
 
 const Resume = ({}) => {
+    const [dropDown, setDropDown] = useState(null);
+
+    const toggleSection = (section) => {
+        setDropDown(dropDown === section ? null : section);
+    }
+
     return (
-        <div className="container">
+    <div className="container">
         <div className="top">
             <Header />
         </div>
         <div className="resume">
-            <div className="wrapper">
+
+            {/* EDUCATION */}
+            <div className="wrapper1" onClick={() => toggleSection('education')}>
                 <div className="section-title">
                     <h2> Education </h2>
                 </div>
             </div>
-            <div className="section">
-                <p>B.S. Computer Science & Engineering</p>
-                <p>Studio Art Minor | Humanitarian Engineering Scholars | 3.4 GPA | Dean's List: SP 22, AU 23, SP 24, AU 24</p>
+            {dropDown === 'education' && (
+                resumeData.education.map((edu, index) => (
+                    <div className="wrapper2" key={index}>
+                        <div className="section1">
+                            <p className="degree">{edu.degree}</p>
+                            <p className="details">{edu.details}</p>
+                        </div>
+                        <div className="section2">
+                            <p className="right">{edu.date}</p>
+                        </div>
+                    </div>
+                ))
+            )}
+
+            {/* EXPERIENCE */}
+            <div className="wrapper1" onClick={() => toggleSection('experience')}>
+                <div className="section-title">
+                    <h2> Experience </h2>
+                </div>
             </div>
+            {dropDown === 'experience' && (
+                resumeData.experience.map((exp, index) => (
+                <div className="wrapper2" key={index}>
+                    <div className="section1">
+                        <p className="degree">{exp.position}</p>
+                        <p className="date">{exp.date}</p>
+                        <ul>
+                            {exp.details.map((detail, index) => (
+                                <li className="details" key={index}>{detail}</li>
+                            ))}
+                        </ul> 
+                        <p className="skills">Skills: {exp.skills}</p>
+                    </div>
+                    <div className="section2">
+                        <p className="right">{exp.location}</p>
+                    </div>
+                </div>
+                ))
+            )}
+
+            {/* PROJECTS */}
+            <div className="wrapper1" onClick={() => toggleSection('projects')}>
+                <div className="section-title">
+                    <h2> Projects </h2>
+                </div>
+            </div>
+            {dropDown === 'projects' && (
+                resumeData.projects.map((proj, index) => (
+                    <div className="wrapper2" key={index}>
+                        <div className="section1">
+                            <p className="degree">{proj.title}</p>
+                            <p className="date">{proj.date}</p>
+                            <ul>
+                                {proj.details.map((detail, index) => (
+                                    <li className="details" key={index}>{detail}</li>
+                                ))}
+                            </ul>
+                            <p className="skills">Skills: {proj.skills}</p>
+                        </div>
+                        <div className="section2">
+                            <p className="right">{proj.location}</p>
+                        </div>
+                    </div>
+                ))
+            )}
         </div>
-        </div>
+    </div>
     );
 }
 
